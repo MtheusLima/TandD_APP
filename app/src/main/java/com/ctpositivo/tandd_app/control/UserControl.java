15deae18845a282f00499c4473f60a11bbb4df1a
Matehus Lima <matheus_pl_ctba@hotmail.com>
2014-11-13 20:46:45 -0200

    first commit

---
+++ app/src/main/java/com/ctpositivo/tandd_app/control/UserControl.java
@@ -0,0 +1,7 @@
+package com.ctpositivo.tandd_app.control;
+
+/**
+ * Created by Matheus on 11/13/2014.
+ */
+public class UserControl {
+}
---
+++ app/src/main/java/com/ctpositivo/tandd_app/model/User.java
@@ -0,0 +1,7 @@
+package com.ctpositivo.tandd_app.model;
+
+/**
+ * Created by Matheus on 11/13/2014.
+ */
+public class User {
+}
